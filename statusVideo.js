695888906fc1ce5fc219812e6d628db5f53dedd4
Tran Van Thi <95225890+Tran02Thi@users.noreply.github.com>
2022-05-31 18:58:09 +0700

    Add files via upload

---
+++ statusVideo.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("status-video");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
